fd9540f1deccd18b1d5d255fe5c316a8f4000008
Higor Souza <higorhenry102@gmail.com>
2024-05-21 14:52:22 -0300

    Cadastra de pedidos nas mesas funcionando OK

--- src/Telas/TelaFazerPedidos/BodyPedidos/Inputs/index.js
+++ src/Telas/TelaFazerPedidos/BodyPedidos/Inputs/index.js
@@ -1,40 +1,49 @@
-import React, {useState} from 'react';
-import { StyleSheet,View, Text, Image, TouchableOpacity, TextInput, Button} from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, Text, TextInput, TouchableOpacity, Alert } from 'react-native';
 import db from '../../../../services/firebaseConfig';
-import {addDoc, collection, query, where, getDocs} from 'firebase/firestore'
-
+import { addDoc, collection, query, where, getDocs } from 'firebase/firestore';
 
 export default function Inputs() {
     const [numeroPedido, setNumeroPedido] = useState('');
     const [numeroMesa, setNumeroMesa] = useState('');
     const [quantidade, setQuantidade] = useState('');
-    const [nomePedido, setNomePedido] = useState('');
-    const [valorPedido, setValorPedido] = useState('');
 
     const onPressFazerPedido = async () => {
-        try {
-            const q = query(collection(db, "Cardapio"), where("NumPedido", "==", numeroPedido));
-            const querySnapshot = await getDocs(q);
-            
-            querySnapshot.forEach((doc) => {
-                const data = doc.data([0]);
-                setNomePedido(data.Nome);
-                setValorPedido(data.Valor);
-            });
-
-            const docData = {
-                NumPedido: numeroPedido,
-                Mesa: numeroMesa,
-                Quantidade: quantidade,
-                NomePedido: nomePedido,
-                ValorPedido: valorPedido
-            };
-
-            await addDoc(collection(db, "Mesas"), docData);
-            alert("Pedido feito com sucesso!");
-            clearInputs();
-        } catch (error) {
-            alert("Erro ao fazer pedido: " + error);
+        if(!numeroPedido == '' && !numeroMesa == '' && !quantidade == '') {
+            try {
+                const numPedido = parseInt(numeroPedido); // Converte para número
+                const q = query(collection(db, "Cardapio"), where("NumPedido", "==", numPedido));
+                const querySnapshot = await getDocs(q);
+    
+                if (!querySnapshot.empty) {
+                    let tempNomePedido = '';
+                    let tempValorPedido = '';
+    
+                    querySnapshot.forEach((doc) => {
+                        const data = doc.data();
+                        tempNomePedido = data.NomePedido;
+                        tempValorPedido = data.Valor;
+                    });
+                    const valorTotal = tempValorPedido * quantidade
+                    const docData = {
+                        NumPedido: numPedido,
+                        Mesa: numeroMesa,
+                        Quantidade: quantidade,
+                        NomePedido: tempNomePedido,
+                        ValorTotal: valorTotal
+                    };
+    
+                    await addDoc(collection(db, "Mesas"), docData);
+                    alert("Pedido feito com sucesso!");
+                    clearInputs();
+                } else {
+                    alert("Pedido não encontrado no banco de dados.");
+                }
+            } catch (error) {
+                alert("Erro ao fazer pedido: " + error);
+            }
+        } else {
+            Alert.alert('Preencha todos os campos!!')
         }
     };
 
@@ -42,12 +51,10 @@ export default function Inputs() {
         setNumeroPedido('');
         setNumeroMesa('');
         setQuantidade('');
-        setNomePedido('');
-        setValorPedido('');
     };
 
     return (
-        <View style={{display:'flex',alignItems: 'center',top: 60,}}>
+        <View style={{ display: 'flex', alignItems: 'center', top: 60 }}>
             <View style={styles.campoInput}>
                 <Text style={styles.txtPedidos}>N. Pedido:</Text>
                 <TextInput
@@ -57,7 +64,7 @@ export default function Inputs() {
                     keyboardType="numeric"
                     value={numeroPedido}
                     onChangeText={setNumeroPedido}
-                ></TextInput>
+                />
             </View>
             <View style={styles.campoInput}>
                 <Text style={styles.txtPedidos}>N. Mesa:</Text>
@@ -68,7 +75,7 @@ export default function Inputs() {
                     keyboardType="numeric"
                     value={numeroMesa}
                     onChangeText={setNumeroMesa}
-                ></TextInput>
+                />
             </View>
             <View style={styles.campoInput}>
                 <Text style={styles.txtPedidos}>Quantidade:</Text>
@@ -79,11 +86,11 @@ export default function Inputs() {
                     keyboardType="numeric"
                     value={quantidade}
                     onChangeText={setQuantidade}
-                ></TextInput>
+                />
             </View>
             <View>
-                <TouchableOpacity style={styles.btFazerPedidos} onPress={onPressFazerPedido} >
-                        <Text style={styles.txtButton}>Fazer Pedido</Text>
+                <TouchableOpacity style={styles.btFazerPedidos} onPress={onPressFazerPedido}>
+                    <Text style={styles.txtButton}>Fazer Pedido</Text>
                 </TouchableOpacity>
             </View>
         </View>
@@ -93,17 +100,15 @@ export default function Inputs() {
 const styles = StyleSheet.create({
     btFazerPedidos: {
         backgroundColor: '#CE7A16',
-        borderRadius: 20,
+        borderRadius: 15,
         width: 250,
         padding: 10,
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
         top: 80,
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.8,
-        shadowRadius: 100,
+        borderWidth: 2,
+        borderColor: "#FFFFFF",
         elevation: 5,
     },
     txtButton: {
@@ -119,8 +124,7 @@ const styles = StyleSheet.create({
         fontSize: 22,
         marginHorizontal: 10,
         color: '#D8D8D8',
-        width:  200
-       
+        width: 200
     },
     campoInput: {
         width: 350,
